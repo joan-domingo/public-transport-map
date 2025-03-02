7a72858ed42f58c05730c870944820ed66ae8285
Joan Domingo <joan.domingo@getflowbox.com>
2025-03-02 23:45:22 +0100

    map improvements

--- src/App.tsx
+++ src/App.tsx
@@ -20,33 +20,26 @@ const App = () => {
 				lng: number;
 		  }
 		| undefined
-		| null
 	>(undefined);
 	console.log('userLocation:', userLocation);
 
-	const getUserLocation = () => {
-		// if geolocation is supported by the users browser
+	const watchPosition = () => {
 		if (navigator.geolocation) {
 			// get the current users location
-			navigator.geolocation.getCurrentPosition(
+			navigator.geolocation.watchPosition(
 				(position) => {
-					console.log('getCurrentPosition:', position);
-					// save the geolocation coordinates in two variables
+					console.log('watch position:', position);
 					const { latitude, longitude } = position.coords;
-					// update the value of userlocation variable
 					setUserLocation({ lat: latitude, lng: longitude });
 				},
-				// if there was an error getting the users location
 				(error) => {
 					console.error('Error getting user location:', error);
-					setUserLocation(null);
 				},
 			);
 		}
 		// if geolocation is not supported by the users browser
 		else {
 			console.error('Geolocation is not supported by this browser.');
-			setUserLocation(null);
 		}
 	};
 
@@ -54,17 +47,17 @@ const App = () => {
 		return <div>Geolocation is not supported by this browser.</div>;
 	}
 
-	if (!busLineId) {
-		return <BusLineSelector />;
-	}
-
 	return (
 		<APIProvider
 			apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
 			language="ca"
-			onLoad={getUserLocation}
+			onLoad={watchPosition}
 		>
-			<Map busLineId={busLineId} userLocation={userLocation} />
+			{!busLineId ? (
+				<BusLineSelector />
+			) : (
+				<Map busLineId={busLineId} userLocation={userLocation} />
+			)}
 		</APIProvider>
 	);
 };
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { Map as GoogleMap } from '@vis.gl/react-google-maps';
+import { AdvancedMarker, Map as GoogleMap } from '@vis.gl/react-google-maps';
 import { useEffect, useState } from 'react';
 import { useShallow } from 'zustand/shallow';
 import appStore from '../store/appStore';
@@ -29,9 +29,22 @@ const Map = ({ busLineId, userLocation }: Props) => {
 		null,
 	);
 
+	const [center, setCenter] = useState<
+		{ lat: number; lng: number } | undefined
+	>({ lat: 41.4912314, lng: 2.1403111 }); // Default: Cerdanyola
+	const [isDragging, setIsDragging] = useState(false);
+
 	useEffect(() => {
 		loadBusStops(busLineId);
 	}, [busLineId]);
+
+	useEffect(() => {
+		if (isDragging) {
+			setCenter(undefined);
+		} else {
+			setCenter(userLocation);
+		}
+	}, [isDragging, userLocation]);
 
 	const handleOnMarkerClick = async (stop: BusStop) => {
 		const {
@@ -44,26 +57,76 @@ const Map = ({ busLineId, userLocation }: Props) => {
 		await loadBusStopTimeTable(ID_PARADA, ID_LINEA, ID_ZONA);
 	};
 
+	const handleOnDrag = () => {
+		setIsDragging(true);
+	};
+
+	console.log('center:', center);
+
 	return (
-		<GoogleMap
-			style={{ width: '100vw', height: '100vh' }}
-			defaultCenter={userLocation || { lat: 41.4912314, lng: 2.1403111 }}
-			defaultZoom={15}
-			// zoom={18}
-			gestureHandling={'greedy'}
-			disableDefaultUI={false}
-			mapId="public-transport-map"
-		>
-			{busLineStops.map((stop) => (
-				<MarkerWithInfowindow
-					key={stop.ID_PARADA}
-					position={{ lat: stop.Parada.LATITUD, lng: stop.Parada.LONGITUD }}
-					onClick={() => handleOnMarkerClick(stop)}
-					visible={openedBusStopMarker === stop.ID_PARADA}
-					onCloseClick={() => setOpenedBusStopMarker(null)}
-				/>
-			))}
-		</GoogleMap>
+		<>
+			<GoogleMap
+				style={{ width: '100vw', height: '100vh' }}
+				center={center}
+				defaultZoom={14}
+				gestureHandling={'greedy'}
+				disableDefaultUI={false}
+				mapId="public-transport-map"
+				onDrag={handleOnDrag}
+			>
+				{busLineStops.map((stop) => (
+					<MarkerWithInfowindow
+						key={stop.ID_PARADA}
+						position={{ lat: stop.Parada.LATITUD, lng: stop.Parada.LONGITUD }}
+						onClick={() => handleOnMarkerClick(stop)}
+						visible={openedBusStopMarker === stop.ID_PARADA}
+						onCloseClick={() => setOpenedBusStopMarker(null)}
+					/>
+				))}
+				{userLocation && (
+					<AdvancedMarker
+						position={userLocation}
+						title={'AdvancedMarker with custom html content.'}
+					>
+						<div
+							style={{
+								width: 16,
+								height: 16,
+								position: 'absolute',
+								top: 0,
+								left: 0,
+								background: 'rgb(50, 47, 213)',
+								border: '2px solid rgba(199, 199, 226, 0.8)',
+								borderRadius: '50%',
+								transform: 'translate(-50%, -50%)',
+							}}
+						/>
+					</AdvancedMarker>
+				)}
+			</GoogleMap>
+			<button
+				onClick={() => {
+					setIsDragging(false);
+					navigator.geolocation.getCurrentPosition((position) => {
+						setCenter({
+							lat: position.coords.latitude,
+							lng: position.coords.longitude,
+						});
+					});
+				}}
+				style={{
+					position: 'absolute',
+					bottom: '10px',
+					left: '10px',
+					padding: '10px',
+					background: 'white',
+					border: 'none',
+					cursor: 'pointer',
+				}}
+			>
+				Recenter
+			</button>
+		</>
 	);
 };
 
